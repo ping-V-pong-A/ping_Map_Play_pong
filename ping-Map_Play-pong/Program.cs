6fdcbe7b4c6f45a3b140c6c64eefafda33a8ef4a
ndrs06 <szaki1006@gmail.com>
2024-05-21 15:16:53 +0200

    chore: project has been started added .gitignore

---
+++ ping-Map_Play-pong/Program.cs
@@ -0,0 +1,29 @@
+using ping_Map_Play_pong.Service.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
