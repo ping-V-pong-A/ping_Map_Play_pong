9117b16baf14c4fec5fbbbd96cfa74c990d926c4
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-05-22 15:00:18 +0200

    chore: added more properties to Table.cs

--- ping-Map_Play-pong/Model/Table.cs
+++ ping-Map_Play-pong/Model/Table.cs
@@ -3,4 +3,8 @@ namespace ping_Map_Play_pong.Model;
 public class Table
 {
     public int Id { get; set; }
+    public string Name { get; set; }
+    public Coordinate Coordinate { get; set; }
+
+    public List<CheckingIn> CheckingIns { get; set; }
 }
