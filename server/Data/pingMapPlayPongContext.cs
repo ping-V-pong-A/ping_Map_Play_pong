070d599d6f9bf75853d281e50dfc7805eed1587d
ndrs06 <szaki1006@gmail.com>
2024-06-04 16:15:54 +0200

    :building_construction: chore: changes implemented due the refactored DataModels

--- server/Data/pingMapPlayPongContext.cs
+++ server/Data/pingMapPlayPongContext.cs
@@ -1,16 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using ping_Map_Play_pong.Model;
 using ping_Map_Play_pong.Model.DataModels;
-using Table = Microsoft.EntityFrameworkCore.Metadata.Internal.Table;
-
+}
 namespace ping_Map_Play_pong.Data;
 
 public class pingMapPlayPongContext : DbContext
 {
-    public DbSet<CheckingIn> CheckingIns { get; set; }
-    public DbSet<Coordinate> Coordinates { get; set; }
-    public DbSet<Table> Tables { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<CheckingIn> CheckingIns { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Coordinate> Coordinates { get; set; }
+    public DbSet<Match> Matches { get; set; }
+    public DbSet<PairMatch> PairMatches { get; set; }
+    
     private readonly IConfiguration _configuration;
 
     public pingMapPlayPongContext(IConfiguration configuration)
@@ -18,25 +21,8 @@ public class pingMapPlayPongContext : DbContext
         _configuration = configuration;
     }
 
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
     {
         optionBuilder.UseSqlServer(_configuration.GetConnectionString("MSSQL_CONNECTION"));
-    }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Entity<User>()
-            .HasIndex(u =>  u.Email )
-            .IsUnique();
-        
-        builder.Entity<User>()
-            .HasIndex(u =>  u.Name )
-            .IsUnique();
-        
-        builder.Entity<User>()
-            .HasMany(u => u.CheckedInTables);
-            
-
     }
 }
