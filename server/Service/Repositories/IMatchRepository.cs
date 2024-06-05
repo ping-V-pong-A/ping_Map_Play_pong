4e50e1f32eb4e7477896ca333dcebc8420ef60a6
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-05 13:52:54 +0200

    :construction: chore: GetAllMethod added to IMatchRepository interface

--- server/Service/Repositories/IMatchRepository.cs
+++ server/Service/Repositories/IMatchRepository.cs
@@ -5,6 +5,7 @@ namespace ping_Map_Play_pong.Service.Repositories;
 public interface IMatchRepository
 {
     IEnumerable<Match> GetByTableId(int tableId);
+    IEnumerable<Match> GetAll();
     IEnumerable<Match> GetByUserId(int userId);
     IEnumerable<Match> GetByPlayer1IdAndPlayer2Id(int player1Id, int player2Id);
     IEnumerable<Match> GetByDate(DateTime date);
