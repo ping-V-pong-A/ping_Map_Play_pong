42d6dab8232070fd4c6651641626e698def0e545
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-06 12:50:22 +0200

    :recycle: refactor: AllTables.jsx refactored

--- client/src/components/AllTables/AllTables.jsx
+++ client/src/components/AllTables/AllTables.jsx
@@ -1,28 +1,32 @@
 import React, {useState, useEffect} from 'react';
 
 
-const [allTables, setAllTables] = useState([])
 
-useEffect(() => {
-    fetch('api/tables')
-        .then(response => response.json())
-        .then(data => setAllTables(data))
-        .catch(error => console.error('Error fetching tables:', error));
-}, []);
+
 
 function AllTables() {
 
+    const [allTables, setAllTables] = useState([])
+    
+    
+    useEffect(() => {
+        fetch('api/tables')
+            .then(response => response.json())
+            .then(data => setAllTables(data))
+            .catch(error => console.error('Error fetching tables:', error));
+    }, []);
+    
 return (
     <>
         <h1>Tables</h1>
-        <ul>
+        {<ul>
             {allTables.map(table => (
                 <li key={table.name}>
                     <p>Latitude: {table.latitude}</p>
                     <p>Longitude: {table.longitude}</p>
                 </li>
             ))}
-        </ul>
+        </ul>}
     </>
 );
 }
