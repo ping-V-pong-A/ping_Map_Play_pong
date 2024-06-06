49cc4fe45c2f0dcfafdd20ece0f49159a256e335
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-06 12:50:02 +0200

    :recycle: refactor: Navbar refactored

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-
-
-
-    return (<>
-    <div className="navbar">
-        <Link to={"/tables"}>
-            <button>Tables        
-        </button>
-    </Link>
-</div>
-</>
-)
-    ;
+    return (
+        <div className="navbar">
+            <Link to="/tables">
+                <button>Tables</button>
+            </Link>
+        </div>
+    );
 }
 
 export default Navbar;
