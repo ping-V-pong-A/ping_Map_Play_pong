6dc560a3d91934885174bc57155c7a8cb80a4d68
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-06 12:05:51 +0200

    :construction: chore: main.jsx added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './index.scss'
+import Tables from './pages/Tables/Tables.jsx'
+import NewTableForm  from "./pages/NewTable/NewTableForm.jsx";
+import Home from './pages/Home/Home.jsx';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+<Tables/>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/tables' element={<Tables/>}/>
+                <Route path='/tables/new' element={<NewTableForm/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+
 )
