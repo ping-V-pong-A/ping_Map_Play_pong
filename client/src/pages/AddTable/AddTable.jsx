368bd8e656c324cbe90ab0709402f77db2bfaafb
ndrs06 <szaki1006@gmail.com>
2024-06-06 17:44:14 +0200

    :sparkles: feat: implemented AddTable page to client side

---
+++ client/src/pages/AddTable/AddTable.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import './NewTableForm.scss';
+
+import TableCreateForm from "../../components/TableCreateForm/TableCreateForm.jsx";
+
+const postTable = (table) => {
+    return fetch(`/api/Table/tables/add`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(table)
+    })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+export default function AddTable() {
+    const navigate = useNavigate();
+    
+    const handleCreateTable = (table) => {
+        postTable(table)
+            .then(_ => navigate("/"))
+    }
+    
+    const props = {
+        onSave: handleCreateTable,
+        onCancel: _ => navigate("/")        
+    }    
+
+    return (
+        <>
+            <TableCreateForm {...props}/>
+        </>
+    );
+}
