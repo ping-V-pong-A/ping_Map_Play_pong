ecae2326a50898c0dda1d8c57f3de10dbed29a42
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-17 15:15:36 +0200

    :construction: chore: UsersContext. cs added to Data folder

---
+++ server/Data/UsersContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ping_Map_Play_pong.Data;
+
+public class UsersContext : IdentityUserContext<IdentityUser>
+{
+    private readonly IConfiguration _configuration;
+    
+    public UsersContext (DbContextOptions<UsersContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = _configuration.GetConnectionString("MSSQL_CONNECTION");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+    
+    
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
