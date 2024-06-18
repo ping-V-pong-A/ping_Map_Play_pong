48249fedf3fd2c531c8c34e44a1760e32efb6229
ndrs <szaki1006@gmail.com>
2024-06-18 15:03:17 +0200

    :bento: feat: created SignIn page

---
+++ client/src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import { useProfile } from '../../contexts/ProfileContext';
+
+import Navbar from "../../components/Navbar/Navbar.jsx";
+import SignInForm from "../../components/SignInForm/SignInForm.jsx";
+
+const postSignIn = (user) => fetch('/api/Auth/Login', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(user)
+    }).then(res => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.ok
+    }).catch(err => {
+        console.error('Error:', err);    
+})
+
+export default function SignIn() {
+    const navigate = useNavigate();
+    const { login } = useProfile();
+
+    const handleSignIn = (user) => {
+        postSignIn(user).then( res => {
+            if (!res)
+            {
+                navigate("/sign-in")
+            }
+            login();
+            navigate("/solar-watch")
+        })
+    }
+
+    const props = {
+        onSave: handleSignIn,
+        onCancel: _ => navigate("/")
+    }
+
+    return (
+        <>
+            <SignInForm {...props}/>
+        </>
+    );
+}
