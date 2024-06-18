4d8bffebc51ec0062e8e7f7a039c2795bbdd437b
ndrs <szaki1006@gmail.com>
2024-06-18 15:04:56 +0200

    :bento: feat: implemented changes into main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,21 +3,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import {ProfileContextProvider} from "./contexts/ProfileContext.jsx";
+import ProtectedRoutes from "./pages/ProtectedRoutes.jsx";
+
+
 import Layout from "./pages/Layout.jsx";
 import Tables from './pages/Tables/Tables.jsx'
 import NewTableForm  from "./pages/AddTable/AddTable.jsx";
 import Home from './pages/Home/Home.jsx';
+import SignIn from "./pages/SignIn/SignIn.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/tables' element={<Tables/>}/>
-                    <Route path='/tables/new' element={<NewTableForm/>}/>
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+        <ProfileContextProvider>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/tables' element={<Tables/>}/>
+                        <Route path='/tables/new' element={<NewTableForm/>}/>
+                        <Route path='/sign-in' element={<SignIn/>}/>
+                        <Route path='/sign-un' element={<SignUp/>}/>
+                        
+                        <Route element={<ProtectedRoutes/>}>
+                            
+                        </Route>
+                        
+                    </Routes>
+                </Layout>
+            </BrowserRouter>            
+        </ProfileContextProvider>
     </React.StrictMode>
 )
