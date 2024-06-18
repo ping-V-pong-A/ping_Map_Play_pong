a74fea38593f8b9607cd7a4f6a3d779648fd6c79
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-18 11:29:39 +0200

    :recycle: refactor: update RegisterAsync for role assignment during user registration in Authservice and its interface

--- server/Service/Authentication/IAuthService.cs
+++ server/Service/Authentication/IAuthService.cs
@@ -2,6 +2,6 @@ namespace ping_Map_Play_pong.Service.Authentication;
 
 public interface IAuthService
 {
-    Task<AuthResult> RegisterAsync(string email, string username, string password);
+    Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
     Task<AuthResult> LoginAsync(string email, string password);
 }
