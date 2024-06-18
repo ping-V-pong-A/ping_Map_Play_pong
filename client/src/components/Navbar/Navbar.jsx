48324a186fdacda3dbdc5bad71e5daa268e92612
ndrs <szaki1006@gmail.com>
2024-06-18 14:07:05 +0200

    :bento: chore: designed a responsive Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import  './Navbar.scss'
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+export default function Navbar() {
+    const [isActive, setIsActive] = useState(false);
+    const handleToggle = () => {
+        setIsActive(!isActive);
+    };
+    
     return (
-        <div className="navbar">
-            <Link to="/tables">
-                <button>Tables</button>
-            </Link>
-        </div>
-    );
+        <nav className="navbar">
+            <div className="navbar__brand">
+                <Link to="/">MyApp</Link>
+            </div>
+            <div className="navbar__toggle" id="navbarToggle" onClick={handleToggle}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={`navbar__links ${isActive ? 'active' : ''}`}>
+                <li><Link to="/" onClick={() => setIsActive(false)}>Home</Link></li>
+                <li><Link to="/tables" onClick={() => setIsActive(false)}>Tables</Link></li>
+                <li><Link to="/tables/new" onClick={() => setIsActive(false)}>New Table</Link></li>
+            </ul>
+        </nav>
+    )
+        ;
 }
-
-export default Navbar;
