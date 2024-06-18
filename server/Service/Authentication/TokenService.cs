6de409530fd7cdbba0cecf407a6c7af477427daf
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-18 07:49:33 +0200

    :construction: chore: added TokenService and ITokenService to Authentication folder

---
+++ server/Service/Authentication/TokenService.cs
@@ -0,0 +1,73 @@
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ping_Map_Play_pong.Service.Authentication;
+
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _configuration;
+    private const int ExpirationMinutes = 30;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    
+    public string CreateToken(IdentityUser user)
+    {
+        var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
+        
+        var issuer = _configuration["JwtSettings:Issuer"];
+        var audience = _configuration["JwtSettings:Audience"];
+        var secretKey = _configuration["JwtSettings:SecretKey"];
+        var token = CreateJwtToken(
+            CreateClaims(user),
+            CreateSigningCredentials(secretKey),
+            expiration,
+            issuer,
+            audience
+        );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+
+    private JwtSecurityToken CreateJwtToken(List<Claim> claims, SigningCredentials credentials,
+        DateTime expiration, string issuer, string audience) =>
+        new JwtSecurityToken(
+            issuer,
+            audience,
+            claims,
+            expires: expiration,
+            signingCredentials: credentials
+        );
+
+    private List<Claim> CreateClaims(IdentityUser user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, "TokenForTheApiWithAuth"),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Iat,
+                EpochTime.GetIntDate(DateTime.UtcNow).ToString(CultureInfo.InvariantCulture),
+                ClaimValueTypes.Integer64),
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+        return claims;
+    }
+
+   
+    private SigningCredentials CreateSigningCredentials(string secretKey)
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+            SecurityAlgorithms.HmacSha256
+        );
+    }
+}
