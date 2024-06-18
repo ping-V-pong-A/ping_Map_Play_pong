105c829c47f1ae26a8c73427b9a3fe966dac3746
ndrs <szaki1006@gmail.com>
2024-06-18 14:09:14 +0200

    :bento: chore: implemented page Layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,23 @@
+import './index.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-//import './index.scss'
+import Layout from "./pages/Layout.jsx";
 import Tables from './pages/Tables/Tables.jsx'
 import NewTableForm  from "./pages/AddTable/AddTable.jsx";
 import Home from './pages/Home/Home.jsx';
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/tables' element={<Tables/>}/>
-                <Route path='/tables/new' element={<NewTableForm/>}/>
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/tables' element={<Tables/>}/>
+                    <Route path='/tables/new' element={<NewTableForm/>}/>
+                </Routes>
+            </Layout>
         </BrowserRouter>
     </React.StrictMode>
-
 )
