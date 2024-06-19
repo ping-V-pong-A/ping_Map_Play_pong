132af773d67f1f227b7428f7e09927808f477592
ndrs <szaki1006@gmail.com>
2024-06-19 13:21:21 +0200

    :bento: chore: add some design to layout

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -1,10 +1,10 @@
 import React  from "react";
-import Navbar from "../components/Navbar/Navbar.jsx";
+import Navbar from "../../components/Navbar/Navbar.jsx";
 import {Outlet} from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div>
+        <div className="layout">
             <Navbar />
             <Outlet />
         </div>
