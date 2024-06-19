eac6cc3937eaee2d06e567af1d87af8d029924e8
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-06-19 13:05:33 +0200

    :recycle: refactor: User class refactored: new foreign key added, and removed name

--- server/Model/DataModels/User.cs
+++ server/Model/DataModels/User.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 using ping_Map_Play_pong.Model.DataModels;
 using ping_Map_Play_pong.Model.Enums;
 
@@ -8,16 +10,17 @@ public class User
 {
     [Key]
     public int Id { get; init; }
-    public string Name { get; init; }
-    [Required]
-    public string Email { get; init; }
-    public string Password { get; init; }
-    public DateTime RegistrationDate { get; init; } = DateTime.Now;
+    
+    public DateTime RegistrationDate { get; init; }
     
     public ICollection<Table> CheckedInTables { get; init; }
     
-    // TODO 
-    // add favorite tables
+
     
+    [ForeignKey("IdentityUserEmail")]
+    public string IdentityUserEmail { get; set; }
+
+    // Navigation property to IdentityUser
+    public IdentityUser IdentityUser { get; set; }
     public Rank Rank { get; init; }
 }
