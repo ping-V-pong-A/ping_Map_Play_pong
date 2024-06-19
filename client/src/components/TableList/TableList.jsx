d7f916a70e3e7b2db826359a9a1b59d019882f86
ndrs <szaki1006@gmail.com>
2024-06-19 13:18:51 +0200

    :recycle: refactor: implemented TableList into Table page

--- client/src/components/TableList/TableList.jsx
+++ client/src/components/TableList/TableList.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export default function AllTables({tables}) {
+export default function TableList({tables}) {
 
     return (
         <>
--- client/src/pages/Tables/Tables.jsx
+++ client/src/pages/Tables/Tables.jsx
@@ -1,21 +1,45 @@
-import React, {useState, useEffect} from 'react';
-import AllTables from '../../components/AllTables/AllTables.jsx'
-import { useNavigate } from "react-router-dom";
 import './Tables.scss'
-function Tables() {
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
 
+import Map from "../../components/Map/Map.jsx";
+import Loading from "../../components/Loading/Loading.jsx";
+import AllTables from '../../components/TableList/TableList.jsx'
+import TableList from "../../components/TableList/TableList.jsx";
+
+const fetchAllTable = () => fetch('/api/Table')
+    .then(resp => resp.json())
+
+export default function Tables() {
     const navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    const [tables, setTables] = useState(null)
+
+    useEffect(() => {
+        fetchAllTable()
+            .then(tables => {
+                setLoading(false)
+                setTables(tables)
+            })
+    }, []);       
+    
     const addNewTableHandler = () =>{
         navigate('/tables/new')
     }
-
-   
     
-    return (<>
-        <AllTables/>
-        <button onClick={addNewTableHandler}>Add new table</button>
-        </>
+    const props = {
+        tables: tables
+    }
+    
+    return (
+        loading ? (
+            <Loading/>
+            ) : (
+            <>
+                <TableList {...props}/>
+                <Map {...props}/>
+                <button onClick={addNewTableHandler}>Add new table</button>
+            </>
+            )
     );
 }
-
-export default Tables;
