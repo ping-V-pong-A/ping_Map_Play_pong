ae2eb1b775ace64eefd058dbeb9f6dd04ff835cf
ndrs <szaki1006@gmail.com>
2024-06-20 18:39:57 +0200

    :fire: chore: deleted Map from Home-page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import './Home.scss';
 import React from 'react';
 
-import Map from "../../components/Map/Map.jsx"
-
 export default function Home() {
     
     return (
-        <><h1>Welcome</h1>
-            <Map/>
+        <>
+            <h1>Welcome</h1>
         </>
     );
 }
