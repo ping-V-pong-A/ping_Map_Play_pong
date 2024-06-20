f6e3da3fe865b32c80ad22880341879b9c2396c7
ndrs <szaki1006@gmail.com>
2024-06-20 18:42:56 +0200

    :building_construction: feat: implemented postCheckIn method and map / list switch button

--- client/src/pages/Tables/Tables.jsx
+++ client/src/pages/Tables/Tables.jsx
@@ -1,21 +1,35 @@
 import './Tables.scss'
 import React, {useState, useEffect} from 'react';
 import { useNavigate } from "react-router-dom";
+import {useProfile} from "../../contexts/ProfileContext.jsx";
+
 import SessionTimeout from "../../components/sessionTimeout/sessionTimeout.js";
 import Map from "../../components/Map/Map.jsx";
 import Loading from "../../components/Loading/Loading.jsx";
-import AllTables from '../../components/TableList/TableList.jsx'
 import TableList from "../../components/TableList/TableList.jsx";
 
 const fetchAllTable = () => fetch('/api/Table')
     .then(resp => resp.json())
 
+const postCheckIn = (checkIn) => fetch('/api/CheckingIn/checkingIns/add', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include',
+    body: JSON.stringify(checkIn)
+})
+    .then((res) => res.json())
+    .catch((error) => console.error('Error:', error))
+
 export default function Tables() {
     
     SessionTimeout();
     const navigate = useNavigate()
+    const {profile} = useProfile();
     const [loading, setLoading] = useState(true)
     const [tables, setTables] = useState(null)
+    const [listMapSwitch, setListMapSwitch] = useState(true)
 
     useEffect(() => {
         fetchAllTable()
@@ -28,9 +42,13 @@ export default function Tables() {
     const addNewTableHandler = () =>{
         navigate('/tables/new')
     }
+    
+   const handleCheckIn = (checkIn) => postCheckIn(checkIn).then(navigate("/tables"))
 
     const props = {
-        tables: tables
+        tables: tables,
+        profile: profile,
+        handleCheckIn: handleCheckIn
     }
 
     return (
@@ -38,9 +56,12 @@ export default function Tables() {
             <Loading/>
         ) : (
             <>
-                <TableList {...props}/>
-                <Map {...props}/>
+                <button onClick={_ => setListMapSwitch(!listMapSwitch)}>List / Map</button>
                 <button onClick={addNewTableHandler}>Add new table</button>
+                {listMapSwitch ?
+                <TableList {...props}/>                    
+                :
+                <Map {...props}/>                }
             </>
         )
     );
