71733896134c00de4ed10eeb39a409568a5da0cd
ndrs <szaki1006@gmail.com>
2024-07-15 14:41:13 +0200

    :necktie: chore: change logic of ProtectedRoutes

--- client/src/pages/ProtectedRoutes.jsx
+++ client/src/pages/ProtectedRoutes.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {Navigate, Outlet, useNavigate} from 'react-router-dom';
 import { useProfile } from '../contexts/ProfileContext.jsx'
 
 export default function ProtectedRoutes() {
-    const { isAuthenticated } = useProfile();
+    const { login, logout } = useProfile();
 
-    return isAuthenticated ? <Outlet /> : <Navigate to="/sign-in" />;
+    const navigate = useNavigate();
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    const logoutTime = localStorage.getItem('logoutTime');
+    
+    useEffect(() => {
+
+        if (!isLoggedIn || (logoutTime && new Date().getTime() > logoutTime)) {
+            localStorage.removeItem('isLoggedIn');
+            localStorage.removeItem('logoutTime');
+            logout();
+        }
+    }, [navigate]);
+
+    return isLoggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
