4b9dbbc3a5ccc7fbed5a9ae7904c6e60f58dcd66
ndrs <szaki1006@gmail.com>
2024-07-16 17:22:51 +0200

    :construction: chore: extended Account page with more details

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -3,35 +3,65 @@ import {useProfile} from "../../contexts/ProfileContext.jsx";
 import Loading from "../../components/Loading/Loading.jsx";
 import {useNavigate} from "react-router-dom";
 
-const getUserData = (id) => fetch(`api/User/users/id/${id*1}/`)
+const getUserData = id => fetch(`api/User/users/id/${id}/`)
+    .then(resp => resp.json())
+    .catch(error => console.error('Error:', error))
+
+const getUserMatches = id => fetch(`api/Match/matches/user/${id}`)
     .then(resp => resp.json())
     .catch(error => console.error('Error:', error))
 
 export default function Account() {
     const navigate = useNavigate();
     const {profile} = useProfile();
+    const [matches, setMatches] = useState(null);
 
     useEffect(() => {
-        getUserData(profile.id)
+        profile ?
+        getUserMatches(profile.id)
+            .then(data => setMatches(data))
+            :
+            navigate('/sign-in')
     }, []);
     
     return profile ? (
         <>
             <button onClick={_ => console.log(profile)}>na</button>
             <div className='container'>
-                <h1>Account</h1>
                 <table>
-                    <tr>
-                        <th>Username</th>
-                        <td>{profile.identityUser.userNamme}</td>
-                    </tr>
-                    <tr>
-                        <th>Email</th>
-                        <td>{profile.email}</td>
-                    </tr>
+                    <thead>
+                        <tr>                            
+                            <th>
+                                <h1>Account</h1>                            
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Username</th>
+                            <td>{profile.identityUser.userName}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>{profile.identityUserEmail}</td>
+                        </tr>
+                        <tr>
+                            <th>Id</th>
+                            <td>{profile.id}</td>
+                        </tr>
+                        {matches ? matches.map(match => (
+                            <tr key={match.id}>
+                                <th>Match {match.id}</th>
+                                <td></td>
+                            </tr>
+                        )) : (
+                            <Loading/>
+                        )}
+                        
+                    </tbody>
                 </table>
             </div>
         </>
-        ) : navigate('/sign-in')
-    
+    ) : navigate('/sign-in')
+
 }
