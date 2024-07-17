f923e063cb9585f118b3466e96deb99123d1bf95
ndrs <szaki1006@gmail.com>
2024-07-17 09:55:12 +0200

    :children_crossing: feat: insert Table page into main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import ProtectedRoutes from "./pages/ProtectedRoutes.jsx";
 
 import Layout from "./pages//Layout/Layout.jsx";
 import Tables from './pages/Tables/Tables.jsx'
+import Table from './pages/Table/Table.jsx'
 import NewTableForm  from "./pages/AddTable/AddTable.jsx";
 import Home from './pages/Home/Home.jsx';
 import SignIn from "./pages/SignIn/SignIn.jsx";
@@ -24,13 +25,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                         <Route path='/' element={<Home/>}/>
                        
                         <Route path='/sign-in' element={<SignIn/>}/>
-                        <Route path='/sign-up' element={<SignUp/>}/>
+                        <Route path='/sign-up' element={<SignUp/>}/>                        
                         
-                        
-                        <Route element={<ProtectedRoutes/>}>
                             <Route path='/tables' element={<Tables/>}/>
+                            <Route path='/tables/table/:id' element={<Table/>}/>
                             <Route path='/tables/new' element={<NewTableForm/>}/>
                             <Route path='/user' element={<Account/>}/>
+                        
+                        <Route element={<ProtectedRoutes/>}>
                         </Route>
                         
                     </Route>
