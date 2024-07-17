0cf25f717a89af79028f7ce9ae0252994dd67967
ndrs <szaki1006@gmail.com>
2024-07-17 09:54:47 +0200

    :children_crossing: chore: formated TableList view

--- client/src/components/TableList/TableList.jsx
+++ client/src/components/TableList/TableList.jsx
@@ -1,30 +1,67 @@
 import React, {useState} from 'react';
 import {Popup} from "react-leaflet";
 import CheckInToTable from "../CheckInToTable/CheckInToTable.jsx";
+import {useNavigate} from "react-router-dom";
 
-export default function TableList({tables,  checkIn, setCheckIn,  handleCheckIn}) {
+export default function TableList({tables, checkIn, setCheckIn, handleCheckIn}) {
+    const navigate = useNavigate();
     const [checkSwitch, setCheckSwitch] = useState({
         id: null,
-        switch: false
+        switch: true
     });
     
     const props = {
         handleCheckIn,
         checkIn,
         setCheckIn,
+        checkSwitch,
+        setCheckSwitch
     }
 
     return (
         <>
-            {
-                checkSwitch.switch && (
-                    <Popup>
-                        <CheckInToTable {...props}/>
-                    </Popup>
-                )
-            }
-                        <h1>Tables</h1>                        
-            <table>
+            {!checkSwitch.switch ? <CheckInToTable{...props}/> : (
+               <ul>
+                   <li>
+                       <table>                           
+                           <thead>
+                           <tr>
+                               <th>Id</th>
+                               <th>Name</th>
+                               <th>CheckIn</th>
+                               <th>######</th>
+                           </tr>
+                           </thead>
+                       </table>
+                   </li>
+                       {tables && tables.map(table => (
+                           <li key={table.id}>
+                               <table>
+                                   <tbody>                                   
+                                       <tr>
+                                           <td>{table.id}</td>
+                                           <td>{table.name}</td>
+                                           <td>
+                                               <button onClick={_ => {
+                                                   setCheckIn({...checkIn, tableId: table.id});
+                                                   setCheckSwitch({...checkSwitch, id: table.id, switch: !checkSwitch.switch})
+                                               }}>checkIn</button>
+                                           </td>
+                                           <td><button onClick={_ => navigate(`table/${table.id}`)}>details</button></td>
+                                       </tr>                                   
+                                   </tbody>
+                               </table>    
+                           </li>
+                       ))}
+                   
+               </ul> 
+                    
+
+            )}
+        </>
+    );
+}
+/*            <table>
                 <thead>
                     <tr>
                         <th><li>Id</li></th>
@@ -39,17 +76,16 @@ export default function TableList({tables,  checkIn, setCheckIn,  handleCheckIn}
                             <td><li>{table.id}</li></td>
                             <td><li>{table.name}</li></td>
                             <td>
-                                <button onClick={_ => (
-                                    setCheckSwitch({...checkSwitch, id: table.id, switch: !checkSwitch.switch})                                    
-                                )}>
+                                <button onClick={_ => {
+                                    setCheckIn({...checkIn, tableId: table.id});
+                                    setCheckSwitch({...checkSwitch, id: table.id, switch: !checkSwitch.switch})
+                                }}>
                                     checkIn
+         
                                 </button>
                             </td>
                             <td><button>details</button></td>
                         </tr> 
                     ))}
                 </tbody>
-            </table>
-        </>
-    );
-}
+            </table>*/
