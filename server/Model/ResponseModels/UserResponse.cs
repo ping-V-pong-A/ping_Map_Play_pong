264dd28fe6f2b1be10c95c6a10c2be559bf5eef7
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-07-17 09:55:56 +0200

    :construction: chore: UserResponse added to ResponseModels

---
+++ server/Model/ResponseModels/UserResponse.cs
@@ -0,0 +1,12 @@
+using ping_Map_Play_pong.Model.DataModels;
+using ping_Map_Play_pong.Model.Enums;
+
+namespace ping_Map_Play_pong.Model.ResponseModels;
+
+public class UserResponse
+{
+    public int Id { get; init; }
+    public DateTime RegistrationDate { get; init; }
+    public ICollection<Table> CheckedInTables { get; init; } = new List<Table>();
+    public Rank Rank { get; init; }
+}
