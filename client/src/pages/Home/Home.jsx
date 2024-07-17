bc556fcd637925d4c6459a7fd8c01efb5449c798
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-07-17 09:47:31 +0200

    :construction: welcoming message added to Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,7 +5,24 @@ export default function Home() {
     
     return (
         <>
-            <h1>Welcome</h1>
+
+            <p> Welcome to Ping Map Play Pong! <br/>
+                This app makes playing ping-pong easier. <br/>
+                You can search for free ping-pong tables near you, check in for a timespan, or add match results.
+                Additionally, you can also record the results of doubles matches.<br/><br/>
+
+                About ping-pong<br/>
+                Playing ping-pong offers numerous benefits for both your physical and mental health. <br/>
+                It’s a fantastic cardiovascular exercise that improves your reflexes, coordination, and balance. The quick movements and
+                strategic thinking involved in the game also help to enhance your cognitive functions and concentration.
+                <br/>
+                Moreover, ping-pong is a social activity that can be enjoyed by people of all ages and skill levels.
+                Whether you're playing casually with friends or competing in tournaments, it's a great way to bond with
+                others and reduce stress.
+                <br/>
+                In short, ping-pong is not only fun and engaging but also a great way to stay active and sharp. So grab
+                a paddle and start playing today!
+            </p>
         </>
     );
 }
