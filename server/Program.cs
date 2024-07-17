4894ed67dab5b508f65e53889e32b3040e7e5153
ndrs <szaki1006@gmail.com>
2024-07-17 15:54:01 +0200

    :recycle: refactor: created service class for CheckingInController

--- server/Program.cs
+++ server/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<PingMapPlayPongContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL_CONNECTION")));
 
-builder.Services.AddScoped<ICheckingInRepository, CheckingInRepository>();
+builder.Services.AddScoped<ICheckingInService, CheckingInService>();
 builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 builder.Services.AddScoped<IPairMatchRepository, PairMatchRepository>();
 builder.Services.AddScoped<ITableService, TableService>();
---
+++ server/Service/ICheckingInService.cs
@@ -0,0 +1,14 @@
+using ping_Map_Play_pong.Model.DataModels;
+using ping_Map_Play_pong.Model.RequestModels;
+
+namespace ping_Map_Play_pong.Service;
+
+public interface ICheckingInService
+{
+    IEnumerable<CheckingIn> GetAll();
+    CheckingIn GetById(int checkingInId);
+    IEnumerable<CheckingIn> GetByUserId(int userId);
+    void PostToDb(CheckInRequest request);
+    void Update(CheckingIn checkingIn);
+    void DeleteFromDb(CheckingIn checkingIn);
+}
