51647a6aa6b61b179bba5f982f08c5a32b3203a9
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2024-07-17 09:49:41 +0200

    :construction: chore: admin route added to Main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,6 +12,7 @@ import NewTableForm  from "./pages/AddTable/AddTable.jsx";
 import Home from './pages/Home/Home.jsx';
 import SignIn from "./pages/SignIn/SignIn.jsx";
 import SignUp from "./pages/SignUp/SignUp.jsx";
+import AdminPage from "./pages/AdminPage/AdminPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -24,7 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                        
                         <Route path='/sign-in' element={<SignIn/>}/>
                         <Route path='/sign-up' element={<SignUp/>}/>
-                        
+                        <Route path='/admin' element={<AdminPage/>}/>
                         <Route element={<ProtectedRoutes/>}>
                             <Route path='/tables' element={<Tables/>}/>
                             <Route path='/tables/new' element={<NewTableForm/>}/>
