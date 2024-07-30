856032b15d3cd69c84c868d14ef3efbbd7d4dcbf
ndrs <szaki1006@gmail.com>
2024-07-30 21:17:16 +0200

    :loud_sound: chore: logs added to MatchService.cs

--- server/Service/MatchService.cs
+++ server/Service/MatchService.cs
@@ -22,19 +22,48 @@ public class MatchService : IMatchService
         _serviceMethods = serviceMethods;
     }
 
-    public IEnumerable<Match> GetAll() => _matchRepository.GetAll();
+    public IEnumerable<Match> GetAll()
+    {
+        _logger.LogInformation("Fetching all matches.");
+        return _matchRepository.GetAll();
+    }
 
-    public Match GetById(int matchId) => _matchRepository.GetById(matchId);
+    public Match GetById(int matchId)
+    {
+        _logger.LogInformation($"Fetching match with id:{matchId}.");
+        var res = _matchRepository.GetById(matchId);
 
-    public IEnumerable<Match> GetByUserId(int userId) => _matchRepository.GetByUserId(userId);
+        if (res == null)
+        {
+            _logger.LogWarning($"Match with ID {matchId} not found.");
+            throw new NotFoundException($"match with id:{matchId} not exist in DB");
+        }
+        return res;
+    }
 
-    public IEnumerable<Match> GetByPlayersId(int player1Id, int player2Id) =>
-        _matchRepository.GetByPlayer1IdAndPlayer2Id(player1Id, player2Id);
+    public IEnumerable<Match> GetByUserId(int userId)
+    {
+        _logger.LogInformation($"Fetching matches for user with ID {userId}.");
+        var res = _matchRepository.GetByUserId(userId).ToList();
 
-    public IEnumerable<Match> GetByDate(DateTime date) => _matchRepository.GetByDate(date);
+        return res;
+    }
 
+    public IEnumerable<Match> GetByPlayersId(int player1Id, int player2Id)
+    {
+        _logger.LogInformation($"Fetching matches for players with IDs {player1Id} and {player2Id}.");
+        return _matchRepository.GetByPlayer1IdAndPlayer2Id(player1Id, player2Id);
+    }
+
+    public IEnumerable<Match> GetByDate(DateTime date)
+    {
+        _logger.LogInformation($"Fetching matches for date {date}.");
+
+        return _matchRepository.GetByDate(date);
+    }
     public void PostToDb(MatchRequest request)
     {
+        _logger.LogInformation("Adding new match to the database.");
         var table = _tableRepository.GetByTableId(request.TableId);
         var player1 = _userRepository.GetById(request.Player1Id);
         var player2 = _userRepository.GetById(request.Player2Id);
@@ -51,14 +80,18 @@ public class MatchService : IMatchService
         };
         
         _matchRepository.Add(newMatch);
+        _logger.LogInformation("Match successfully added to the database.");
+
     }
 
     public void Update(int matchId, MatchRequest request)
     {
+        _logger.LogInformation($"Updating match with ID {matchId}.");
         var match = _matchRepository.GetById(matchId);
 
         if (match == null)
         {
+            _logger.LogWarning($"Match with ID {matchId} not found.");
             throw new NotFoundException("404");
         }
         
@@ -77,18 +110,22 @@ public class MatchService : IMatchService
         _serviceMethods.UpdateProperties(request, match);
         
         _matchRepository.Update(match);
+        _logger.LogInformation($"Match with ID {matchId} successfully updated.");
     }
 
     public void DeleteFromDb(int matchId)
     {
+        _logger.LogInformation($"Deleting match with ID {matchId} from the database.");
         var match = _matchRepository.GetById(matchId);
 
         if (match == null)
         {
+            _logger.LogWarning($"Match with ID {matchId} not found.");
             throw new NotFoundException("404");
         }
         
         _matchRepository.Delete(match);
+        _logger.LogInformation($"Match with ID {matchId} successfully deleted from the database.");
     }
 
 }
