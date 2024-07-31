efc127143cd1c048145a0a3de5ed1724d6508968
ndrs <szaki1006@gmail.com>
2024-07-31 11:57:21 +0200

    :building_construction: chore: implemented req, resp models in UserService

--- server/Service/UserService.cs
+++ server/Service/UserService.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
-using ping_Map_Play_pong.Model;
+using ping_Map_Play_pong.Model.Exceptions;
 using ping_Map_Play_pong.Model.RequestModels;
+using ping_Map_Play_pong.Model.ResponseModels;
 using ping_Map_Play_pong.Service.Repositories;
 
 namespace ping_Map_Play_pong.Service;
@@ -18,20 +19,73 @@ public class UserService : IUserService
         _userManager = userManager;
     }
 
-    public IEnumerable<User> GetAll() => _userRepository.GetAll();
-
-    public User GetById(int userId) => _userRepository.GetById(userId);
-
-    public void Update(UserRequest request)
+    public IEnumerable<UserResponse> GetAll()
     {
-        // TODO
-        throw new NotImplementedException();
+        var users = _userRepository.GetAll();
+            
+        var respUsers = users.Select(user => new UserResponse
+        {
+            Id = user.Id,
+            RegistrationDate = user.RegistrationDate,
+            CheckedInTables = user.CheckedInTables.ToList(),
+            Rank = user.Rank
+        }).ToList();
+
+
+        return respUsers;
     }
 
-    public async void Delete(User user)
+    public UserResponse GetById(int userId)
     {
-        var identityUser = await _userManager.FindByEmailAsync(user.IdentityUserEmail);
+        var user = _userRepository.GetById(userId);
+
+        var respUser = new UserResponse()
+        {
+            UserName = user.IdentityUser.UserName
+        };
+
+        return respUser;
+    }
+
+    public UserResponse GetByEmail(string userEmail)
+    {
+        var user = _userRepository.GetByEmail(userEmail);
+
+        var respUser = new UserResponse()
+        {
+            Id = user.Id,
+            UserName = user.IdentityUser.UserName,
+            Rank = user.Rank,
+            RegistrationDate = user.RegistrationDate
+        };
+
+        return respUser;
+    }
+
+    public void Update(int userId, UserRequest request)
+    {
+        var user = _userRepository.GetById(userId);
+            
+        if (user == null)
+        {
+            throw new NotFoundException($"user not found with Id:{userId}");
+        }
+
+        user.Rank = request.Rank;
         
+        _userRepository.Update(user);
+    }
+
+    public async void Delete(int userId)
+    {
+        var user = _userRepository.GetById(userId);
+
+        if (user == null)
+        {
+            throw new NotFoundException($"user not found with Id:{userId}");
+        }
+
+        var identityUser = await _userManager.FindByEmailAsync(user.IdentityUserEmail);
         await _userManager.DeleteAsync(identityUser);
     }
 
