7ff3b236a8ce34b1e61e90ba1fd8f253595af19a
ndrs <szaki1006@gmail.com>
2024-07-31 11:54:29 +0200

    :building_construction: chore: implemented req, resp models in TableService

--- server/Service/ITableService.cs
+++ server/Service/ITableService.cs
@@ -1,12 +1,14 @@
 using ping_Map_Play_pong.Model.DataModels;
+using ping_Map_Play_pong.Model.RequestModels;
+using ping_Map_Play_pong.Model.ResponseModels;
 
 namespace ping_Map_Play_pong.Service;
 
 public interface ITableService
 {
-    IEnumerable<Table> GetAll();
+    IEnumerable<TableResponse> GetAll();
     Table GetById(int tableId);
-    void PostToDb(Table table);
-    void Update(Table table);
+    void PostToDb(TableRequest request);
+    void Update(int tableId, TableRequest request);
     void Delete(int tableId);
 }
--- server/Service/TableService.cs
+++ server/Service/TableService.cs
@@ -1,4 +1,7 @@
 using ping_Map_Play_pong.Model.DataModels;
+using ping_Map_Play_pong.Model.Exceptions;
+using ping_Map_Play_pong.Model.RequestModels;
+using ping_Map_Play_pong.Model.ResponseModels;
 using ping_Map_Play_pong.Service.Repositories;
 
 namespace ping_Map_Play_pong.Service;
@@ -14,13 +17,71 @@ public class TableService : ITableService
         _tableRepository = tableRepository;
     }
 
-    public IEnumerable<Table> GetAll() => _tableRepository.GetAll();
+    public IEnumerable<TableResponse> GetAll()
+    {
+        var tables = _tableRepository.GetAll();
+            
+        var respTables = tables.Select(table => new TableResponse
+        {
+            Id = table.Id,
+            Name = table.Name,
+            Lat = table.Coordinate.Lat,
+            Lon = table.Coordinate.Lon,
+            CheckingIns = table.CheckingIns.ToList(),
+            Matches = table.LeaderBoard.ToList(),
+            PairMatches = table.PairMatchesLeaderBoard.ToList()
+        }).ToList();
+        
+        return respTables;
+    }
 
-    public Table GetById(int tableId) => _tableRepository.GetByTableId(tableId);
+    public Table GetById(int tableId)
+    {
+        return _tableRepository.GetById(tableId);
+    }
 
-    public void PostToDb(Table table) => _tableRepository.Add(table);
+    public void PostToDb(TableRequest request)
+    {
+        var coordinate = new Coordinate
+        {
+            Lat = request.Lat,
+            Lon = request.Lon
+        };
+        
+        var newTable = new Table
+        {
+            Name = request.Name,
+            Coordinate = coordinate
+        };
+        
+        _tableRepository.Add(newTable);
+    }
 
-    public void Update(Table table) => _tableRepository.Update(table);
+    public void Update(int tableId, TableRequest request)
+    {
+        var table = _tableRepository.GetById(tableId);
+        
+        if (table == null)
+        {
+            throw new NotFoundException($"table with id:{tableId} not exist in DB");
+        }
 
-    public void Delete(int tableId) => _tableRepository.Delete(_tableRepository.GetByTableId(tableId));
+        table.Name = request.Name;
+        table.Coordinate.Lat = request.Lat;
+        table.Coordinate.Lon = request.Lon;
+        
+        _tableRepository.Update(table);
+    }
+
+    public void Delete(int tableId)
+    {
+        var table = _tableRepository.GetById(tableId);
+        
+        if (table == null)
+        {
+            throw new NotFoundException($"table with id:{tableId} not exist in DB");
+        }
+        
+        _tableRepository.Delete(table);
+    }
 }
